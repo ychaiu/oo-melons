4fee55db0ef6f327d197a7e414c2659d14dd3263
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-09 20:59:23 -0700

    complete part 3: create GovernmentMelonOrder class and create function for inspection

--- melons.py
+++ melons.py
@@ -14,15 +14,11 @@ class AbstractMelonOrder():
         """Calculate price, including tax."""
 
         base_price = 5
-        
         if self.species == "christmas":
             base_price *= 1.5
-        
         total = (1 + self.tax) * self.qty * base_price
-        
         if self.order_type == "international" and self.qty < 10:
             total += 3
-
         return total
 
     def mark_shipped(self):
@@ -49,3 +45,15 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """US Government melon order."""
+
+    tax = 0.00
+    passed_inspection = False
+
+    def mark_inspection(self, passed):
+    """Changes passed_inspection if True"""
+         
+        self.passed_inspection = passed
+
